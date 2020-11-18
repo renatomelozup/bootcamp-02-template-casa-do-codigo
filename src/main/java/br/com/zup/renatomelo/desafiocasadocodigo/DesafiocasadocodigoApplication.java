31861dd6d0c14fdf223294b6f27b4e62c19a5964
Renato Pessoa <renato.melo@zup.com.br>
2020-11-18 17:44:43 -0300

    initial commit

---
+++ src/main/java/br/com/zup/renatomelo/desafiocasadocodigo/DesafiocasadocodigoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.renatomelo.desafiocasadocodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiocasadocodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiocasadocodigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/renatomelo/desafiocasadocodigo/DesafiocasadocodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.renatomelo.desafiocasadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiocasadocodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
